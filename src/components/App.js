96dd5cd183fd40dbe4a68698061a8bb5d3027408
Ben Adam <bxadam@godaddy.com>
2017-04-17 21:26:29 -0700

    More scalable loading solution

--- src/components/App.js
+++ src/components/App.js
@@ -4,28 +4,24 @@ import Message from './Message';
 import Parallax from './Parallax';
 import Loader from './Loader';
 import '../assets/css/App.css';
+import sprite from '../assets/images/sprite.jpg';
 
 class App extends Component {
   constructor() {
     super();
     this.updateScroll = this.updateScroll.bind(this);
     this.handleResize = this.handleResize.bind(this);
+    this.handleLoad = this.handleLoad.bind(this);
     this.state = { scrollDistance: 0, multiplier: 60, isLoading: true };
   }
 
   componentDidMount() {
     window.addEventListener('scroll', this.updateScroll, false);
     window.addEventListener('resize', this.handleResize, false);
+    this.image = new Image();
+    this.image.src = sprite;
+    this.image.onload = this.handleLoad;
     this.handleResize();
-    this.checkLastClear();
-    if (!localStorage.cached) {
-      localStorage.setItem('cached', true);
-      setTimeout(() => {
-        this.setState({ isLoading: false });
-      }, 4000);
-      return;
-    }
-    this.setState({ isLoading: false });
   }
 
   componentWillUnmount() {
@@ -33,27 +29,20 @@ class App extends Component {
     window.removeEventListener('resize', this.handleResize);
   }
 
-  checkLastClear() {
-    let lastclear = localStorage.getItem('lastclear') || 0;
-    let time_now = new Date().getTime();
-    if (time_now - lastclear > 1000 * 60 * 60 * 24) { 
-      localStorage.clear();
-      localStorage.setItem('lastclear', time_now);
-    }
-  }
-
   updateScroll() {
     const { multiplier } = this.state;
     const distance = this.numberBetween(window.scrollY / multiplier).toFixed();
-    this.setState({
-      scrollDistance: this.numberBetween(distance)
-    });
+    this.setState({ scrollDistance: this.numberBetween(distance) });
   }
 
   handleResize() {
     this.setState({
       isMobile: window.matchMedia('(max-width: 600px)').matches
     });
+  }
+
+  handleLoad() {
+    this.setState({ isLoading: false });
   }
 
   numberBetween(number) {
