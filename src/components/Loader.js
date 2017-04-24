63c3421dcfc01e42354b73b1016071be6f6fdd3d
Ben Adam <bxadam@godaddy.com>
2017-04-23 21:57:28 -0700

    Tweaks

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,9 +2,7 @@ import { h } from 'preact';
 
 const Loader = props => (
   <div className="loader-wrapper">
-    <h1 className="loader-text">
-      Hang tight ...
-    </h1>
+    <h1 className="loader-text">Hang tight...</h1>
   </div>
 );
 
